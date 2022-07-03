f289f7b25a6b9b52411be99ed901d38258b52315
Aarti212 <nayak.aarti212@gmail.com>
2022-07-03 23:32:45 +0530

    ok

--- src/main/java/org/example/StreamNumber.java
+++ src/main/java/org/example/StreamNumber.java
@@ -10,6 +10,7 @@ import java.util.stream.IntStream;
 public class StreamNumber {
     private static final Logger LOGGER = LogManager.getLogger(StreamNumber.class);
 
+    static StreamNumber StreamNum=new StreamNumber();
     public static void main(String[] args) {
         LOGGER.info("Program starts");
         try {
@@ -20,12 +21,13 @@ public class StreamNumber {
             list.add(7);
             list.add(9);
             list.add(16);
-            List<Integer> evenList = list.stream().filter(i -> i % 2 == 0).collect(Collectors.toList());
+            List<Integer> evenList = StreamNum.findEven(list);
             LOGGER.debug("even numbers using stream are: " + evenList);
 
-            List<Integer> oddList = list.stream().filter(i -> i % 2 != 0).collect(Collectors.toList());
+            List<Integer> oddList = StreamNum.findOdd(list);
             LOGGER.debug("odd numbers using stream are: " + oddList);
-            List<Integer> primeList = list.stream().filter(StreamNumber::isPrime).collect(Collectors.toList());
+
+            List<Integer> primeList = StreamNum.findPrime(list);
             LOGGER.debug("odd numbers using stream are: " + primeList);
         }
         catch (Exception e){
@@ -36,6 +38,19 @@ public class StreamNumber {
             LOGGER.info("End of Program");
         }
     }
+    public  List<Integer> findEven(List<Integer> list ){
+        List<Integer> evenList=list.stream().filter(i -> i % 2 == 0).collect(Collectors.toList());
+        return evenList;
+    }
+    public List<Integer> findOdd(List<Integer> list ){
+        List<Integer> oddList=list.stream().filter(i -> i % 2 != 0).collect(Collectors.toList());
+        return oddList;
+    }
+    public List<Integer> findPrime(List<Integer> list ){
+        List<Integer> primeList = list.stream().filter(StreamNumber::isPrime).collect(Collectors.toList());
+        return primeList;
+    }
+
     public static boolean isPrime ( int number){
         return IntStream.rangeClosed(2, number / 2).noneMatch(i -> number % i == 0);
     }
